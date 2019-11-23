33c68ce38514ffda745182fef35c50272cbef93a
Walim ECHAIB <walim.echaib.dz@viacesi.fr>
2019-11-23 17:42:14 +0100

    Ajoutez des fichiers projet.

---
+++ ProjetPrograSys/Program.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace ProjetPrograSys
+{
+    class Program
+    {
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            // Add file sizes.
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+            }
+            return size;
+        }
+        static void Main(string[] args)
+        {
+            int test = 200;
+            DateTime now = DateTime.Now;
+            string dateT = now.ToString("dd MMMM yyyy hh:mm:ss tt");
+            string path = @"C:\Users\ASUS\Desktop\Backup";
+            Console.WriteLine("Entrez le nom de la tâche : ");
+            string task = Console.ReadLine();
+            Console.WriteLine("Veuillez spécifier le chemin");
+            string srcPathDir = Console.ReadLine();
+            string srcPath = @"C:\Users\ASUS\Desktop\"+srcPathDir;
+            Console.WriteLine("Vous avez choisi le chemin : " + srcPath);
+
+            StoreLogs Logs = new StoreLogs()
+            {
+                Id = 1,
+                DateTime = dateT,
+                TaskName = task,
+                SrcAddress = srcPath,
+                DstAddress = path,
+                FileSize = DirSize(new DirectoryInfo(path)),
+                DelayTransfer = 10,
+            };
+
+            // Record JSON data in the variable
+            string strResultJson = JsonConvert.SerializeObject(Logs);
+
+            // Show the JSON Data
+            Console.WriteLine(strResultJson);
+
+            // Write JSON Data in another file
+            //File.WriteAllText(@"C:\Users\ASUS\Desktop\Backup\.json", strResultJson);
+
+            //Logs.StoreData(path, srcPath);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProjetPrograSys/StoreLogs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ProjetPrograSys
+{
+    class StoreLogs
+    {
+        public int Id { get; set; }
+        public string DateTime { get; set; }
+        public string TaskName { get; set; }
+        public string SrcAddress { get; set; }
+        public string DstAddress { get; set; }
+        public double FileSize { get; set; }
+        public double DelayTransfer { get; set; }
+        public string path { get; set; }
+
+        
+
+        public void StoreData(string path, string srcPath)
+        {
+            // Specify the directory you want to manipulate.
+            /*string path = @"C:\Users\ASUS\Desktop\Backup\"*/;
+
+            DirectoryInfo diSource = new DirectoryInfo(path);
+            DirectoryInfo diTarget = new DirectoryInfo(srcPath);
+
+            try
+            {
+                // Determine whether the directory exists.
+                if (Directory.Exists(path))
+                {
+                    Console.WriteLine("That path exists already.");
+                    return;
+                }
+
+                // Try to create the directory.
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
+
+                // Delete the directory.
+                //di.Delete();
+                //Console.WriteLine("The directory was deleted successfully.");
+
+                //CopyFiles of the directory
+                
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            finally { }
+
+        }
+    }
+ }
