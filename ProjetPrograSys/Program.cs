2fa9f7ee4435c6a54a79483fad0dd7bafcd47ea6
Walim ECHAIB <walim.echaib.dz@viacesi.fr>
2019-11-25 11:32:45 +0100

    Time transfer added

--- ProjetPrograSys/Program.cs
+++ ProjetPrograSys/Program.cs
@@ -1,11 +1,13 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Diagnostics;
 
 namespace ProjetPrograSys
 {
     class Program
     {
+        delegate long MyDelegate(long size);
         public static long DirSize(DirectoryInfo d)
         {
             long size = 0;
@@ -23,75 +25,67 @@ namespace ProjetPrograSys
             }
             return size;
         }
-
-        public static void Copy(string path, string srcPath)
-        {
-            var diSource = new DirectoryInfo(srcPath);
-            var diTarget = new DirectoryInfo(path);
-
-            CopyAll(diSource, diTarget);
-            Console.WriteLine(srcPath+path);
-        }
-
-        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
-        {
-            Directory.CreateDirectory(target.FullName);
-
-            // Copy each file into the new directory.
-            foreach (FileInfo fi in source.GetFiles())
-            {
-                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
-                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
-            }
-
-            // Copy each subdirectory using recursion.
-            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
-            {
-                DirectoryInfo nextTargetSubDir =
-                    target.CreateSubdirectory(diSourceSubDir.Name);
-                CopyAll(diSourceSubDir, nextTargetSubDir);
-            }
-        }
         static void Main(string[] args)
-        {      
+        {
+            Console.WriteLine("Enter the work ID : ");
+            string ID = Console.ReadLine();
+
+            // Create a DateTime object to catch the time
             DateTime now = DateTime.Now;
             string dateT = now.ToString("dd MMMM yyyy hh:mm:ss tt");
 
             string path = @"C:\Users\ASUS\Desktop\Backup";
-            Console.WriteLine("Entrez le nom de la tâche : ");
+            Console.WriteLine("Enter the name of the task : ");
 
+            // Catch the source file
             string task = Console.ReadLine();
-            Console.WriteLine("Veuillez spécifier le chemin");
+            Console.WriteLine("Specify the path : ");
             
             string srcPathDir = Console.ReadLine();
             string srcPath = @"C:\Users\ASUS\Desktop\"+srcPathDir;
-            Console.WriteLine("Vous avez choisi le chemin : " + srcPath);
+            Console.WriteLine("You choosed this path : " + srcPath);
 
-            StoreLogs Logs = new StoreLogs()
+            StoreLogs Logs = new StoreLogs();
+
+            // Verify if a Backup directory exist and create it if not
+            Logs.CreateDir(path, srcPath);
+
+            // Create a stopWatch object
+            Stopwatch stopWatch = new Stopwatch();
+           
+            // Start the countdown
+            stopWatch.Start();
+                // Copy files from the source directory to the destination
+                Logs.Copy(path, srcPath);
+            // Stop the countdown
+            stopWatch.Stop();
+
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Create an object and store properties
+            StoreLogs Log = new StoreLogs()
             {
-                Id = 1,
+                Id = ID,
                 DateTime = dateT,
                 TaskName = task,
                 SrcAddress = srcPath,
                 DstAddress = path,
                 FileSize = DirSize(new DirectoryInfo(path)),
-                DelayTransfer = 10,
+                DelayTransfer = ts.Milliseconds,
             };
 
             // Record JSON data in the variable
-            string strResultJson = JsonConvert.SerializeObject(Logs);
+            string strResultJson = JsonConvert.SerializeObject(Log);
 
             // Show the JSON Data
-            Console.WriteLine(strResultJson);
+            // Console.WriteLine(strResultJson);
 
             // Write JSON Data in another file
-            File.WriteAllText(@"C:\Users\ASUS\Desktop\Backup\logs.json", strResultJson);
-
-            // Verify if a Backup directory exist and create it if not
-            Logs.CreateDir(path, srcPath);
+            File.WriteAllText(@"C:\Users\ASUS\Desktop\Backup\logs\log.json", strResultJson);
             
-            // Copy files from the source directory to the destination
-            Copy(path, srcPath);
+            Console.WriteLine("JSON Object generated !");
+
 
             Console.ReadLine();
         }
--- ProjetPrograSys/StoreLogs.cs
+++ ProjetPrograSys/StoreLogs.cs
@@ -7,15 +7,43 @@ namespace ProjetPrograSys
 {
     class StoreLogs
     {
-        public int Id { get; set; }
+        public String Id { get; set; }
         public string DateTime { get; set; }
         public string TaskName { get; set; }
         public string SrcAddress { get; set; }
         public string DstAddress { get; set; }
         public double FileSize { get; set; }
         public double DelayTransfer { get; set; }
-        public string path { get; set; }
+        // public string path { get; set; }
 
+
+        public void Copy(string path, string srcPath)
+        {
+            var diSource = new DirectoryInfo(srcPath);
+            var diTarget = new DirectoryInfo(path);
+
+            CopyAll(diSource, diTarget);
+        }
+
+        public void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
 
         public void CreateDir(string path, string srcPath)
         {
