44eebf8114f4cab307d4b03d5083755196ae4571
Walim ECHAIB <walim.echaib.dz@viacesi.fr>
2019-11-27 23:10:09 +0100

    Fixed Size function

--- ProjetPrograSys/StoreLogs.cs
+++ ProjetPrograSys/StoreLogs.cs
@@ -20,13 +20,13 @@ namespace ProjetPrograSys
         {
             long size = 0;
             // Add file sizes.
-            FileInfo[] fis = d.GetFiles();
+            FileInfo[] fis = strPath.GetFiles();
             foreach (FileInfo fi in fis)
             {
                 size += fi.Length;
             }
             // Add subdirectory sizes.
-            DirectoryInfo[] dis = d.GetDirectories();
+            DirectoryInfo[] dis = strPath.GetDirectories();
             foreach (DirectoryInfo di in dis)
             {
                 size += DirSize(di);
