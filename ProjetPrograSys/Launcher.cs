3c0812fe878f5b19611cc1a2208518216cdc9695
Walim ECHAIB <walim.echaib.dz@viacesi.fr>
2019-11-27 15:54:35 +0100

    Menu added

---
+++ ProjetPrograSys/Launcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetPrograSys
+{
+    class Launcher
+    {
+        public void starter()
+        {
+            Console.WriteLine("####################################################################################################################");
+            Console.WriteLine(@"#                              ______                   _____                                                      #");
+            Console.WriteLine(@"#                             |  ____|                 / ____|                                                     #");
+            Console.WriteLine(@"#                             | |__   __ _ ___ _   _  | (___   __ ___   _____                                      #");
+            Console.WriteLine(@"#                             |  __| / _` / __| | | |  \___ \ / _` \ \ / / _ \                                     #");
+            Console.WriteLine(@"#                             | |___| (_| \__ \ |_| |  ____) | (_| |\ V /  __/                                     #");
+            Console.WriteLine(@"#                             |______\__,_|___/\__, | |_____/ \__,_| \_/ \___|                                     #");
+            Console.WriteLine(@"#                                               __/ |                                                              #");
+            Console.WriteLine(@"#                                              |___/                                                               #");
+            Console.WriteLine(@"#                                                                                                                  #");
+            Console.WriteLine("####################################################################################################################");
+            Console.WriteLine("#  Number  #            Options                                                                                    #");
+            Console.WriteLine("####################################################################################################################");
+            Console.WriteLine("#    1     #            Add a new save task                                                                        #");
+            Console.WriteLine("####################################################################################################################");
+            Console.WriteLine("#    2     #            Launch a specific save task                                                                #");
+            Console.WriteLine("####################################################################################################################");
+            Console.WriteLine("#    3     #            Launch a sequence of save tasks                                                            #");
+            Console.WriteLine("####################################################################################################################");
+            Console.WriteLine("#    4     #            Consult save tasks’ list                                                                   #");
+            Console.WriteLine("####################################################################################################################");
+            Console.WriteLine("#    5     #            Exit                                                                                       #");
+            Console.WriteLine("####################################################################################################################");
+        }
+    }
+}
